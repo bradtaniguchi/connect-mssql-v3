391709abd581a0c4c91f7537f952ad0245f9fe5e
Brad Taniguchi <bradtaniguchi@gmail.com>
2025-05-25 19:54:00 -0700

    fix: add basic devcontainer setting

--- src/store.spec.ts
+++ src/store.spec.ts
@@ -1,7 +1,25 @@
-import test from "node:test";
+import { config, ConnectionPool } from "mssql";
 
-describe("hello world", () => {
-  test("test", () => {
-    expect(true).toEqual(true);
-  });
+const sqlConfig: config = {
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  server: process.env.SQL_SERVER ?? "",
+  database: process.env.SQL_DATABASE,
+  options: { trustServerCertificate: true },
+};
+
+beforeAll(async () => {
+  const db = new ConnectionPool(sqlConfig);
+  await db.connect();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const request = db.request();
+
+  // TODO: do some sanity testing before hand
+  // await request.query("DELETE FROM dbo.Sessions");
+  // await db.close();
+});
+
+describe("connect-mssql-v3", () => {
+  // TODO: write some tests
 });
